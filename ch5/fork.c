b1866e90e5ab128e1a446d8eb0a685da519374e2
Marhtini <JohnMartinez90@gmail.com>
2024-11-20 19:35:08 -0500

    Fixing git config.

--- ch5/fork.c
+++ ch5/fork.c
@@ -4,6 +4,10 @@
 
 int main(){
 
+    int x = 100;
+
+    printf("x is set to %d\n", x);
+
     pid_t p = fork();
 
     if (p < 0){
@@ -11,8 +15,18 @@ int main(){
         exit(1);
     }
 
+    else if (p == 0){
+        printf("Child: process_id(pid) = %d\n", getpid());
+        printf("The int x is: %d\n", x);
+        x = 200;
+        printf("Child: Changed x to %d\n", x);
+    }
+
     else {
-        printf("New Process Created: , process_id(pid) = %d\n", getpid());
+        printf("Parent: process_id(pid) = %d\n", getpid());
+        printf("Parent: x = %d\n", x);
+        x = 300;
+        printf("Parent: Changed x to %d\n", x);
     }
 
     return 0;
